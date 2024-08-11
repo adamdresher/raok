212a372195f5d09a7b8a33a115356796089ac020
Adam Dresher <adamdresher@gmail.com>
2024-08-11 14:13:20 +0700

    update(test): make test user data generic ; update interface to create more test users

--- test/test_app.rb
+++ test/test_app.rb
@@ -21,16 +21,16 @@ class RAOKTest < Minitest::Test
     session[:current_user] = 1 # this requires an 'admin' user in the database
   end
 
-  def create_admin_user
-    user_data = ['admin name',
-                 'admin@test.com',
-                 'admin',
-                 BCrypt::Password.create('P4ssword')]
+  def create_user(name: 'name', email: 'email@test.com', username: 'username', password: 'P4ssword')
+    user_data = [name,
+                 email,
+                 username,
+                 BCrypt::Password.create(password)]
  
     @storage.add_user!(user_data)
   end
 
-  def add_admin_user_posts
+  def add_user_posts
   end
 
   def setup
@@ -44,13 +44,13 @@ class RAOKTest < Minitest::Test
 
   # Tests
   def test_signup_success
-    message = '<p>Congrats admin name, your account was created'
+    message = '<p>Congrats name, your account was created'
 
-    post '/signup', { username: 'admin',
+    post '/signup', { name: 'name',
+                      email: 'email@test.com',
+                      username: 'username',
                       password1: 'P4ssword',
-                      password2: 'P4ssword',
-                      name: 'admin name',
-                      email: 'admin@test.com' }
+                      password2: 'P4ssword' }
 
     assert_equal 302, last_response.status
     get last_response["Location"] # redirects for 302 status code
@@ -61,11 +61,11 @@ class RAOKTest < Minitest::Test
   def test_signup_fail_passwords_dont_match
     message = '<p>Password unaccepted, repeat the same password twice'
 
-    post '/signup', { username: 'admin',
+    post '/signup', { name: 'name',
+                      email: 'email@test.com',
+                      username: 'username',
                       password1: 'P4ssword',
-                      password2: 'different password',
-                      name: 'admin name',
-                      email: 'admin@test.com' }
+                      password2: 'different password' }
 
     assert_equal 302, last_response.status
     get last_response["Location"] # redirects for 302 status code
@@ -76,36 +76,36 @@ class RAOKTest < Minitest::Test
   def test_signup_fail_invalid_password
     message = '<p>Password must be 6 or more characters, including uppercase and lowercase letters and a number'
 
-    post '/signup', { username: 'admin',
+    post '/signup', { name: 'name',
+                      email: 'email@test.com',
+                      username: 'username',
                       password1: 'password',
-                      password2: 'password',
-                      name: 'admin name',
-                      email: 'admin@test.com' }
+                      password2: 'password' }
 
     assert_equal 302, last_response.status
     get last_response["Location"]
 
     assert_includes last_response.body, message
 
-    post '/signup', { username: 'admin',
+    post '/signup', { name: 'name',
+                      email: 'email@test.com',
+                      username: 'username',
                       password1: 'Password',
-                      password2: 'Password',
-                      name: 'admin name',
-                      email: 'admin@test.com' }
+                      password2: 'Password' }
     get last_response["Location"]
 
     assert_includes last_response.body, message
   end
 
   def test_signup_fail_user_exists
-    create_admin_user
+    create_user
     message = '<p>Username is already taken, choose a new username'
 
-    post '/signup', { username: 'admin',
+    post '/signup', { name: 'name',
+                      email: 'new_email@test.com',
+                      username: 'username',
                       password1: 'P4ssword',
-                      password2: 'P4ssword',
-                      name: 'admin name',
-                      email: 'a@test.com' }
+                      password2: 'P4ssword' }
 
     assert_equal 302, last_response.status
     get last_response["Location"] # redirects for 302 status code
@@ -114,14 +114,14 @@ class RAOKTest < Minitest::Test
   end
 
   def test_signup_fail_email_exists
-    create_admin_user
+    create_user
     message = '<p>Email address is already registered'
 
-    post '/signup', { username: 'username',
+    post '/signup', { name: 'name',
+                      email: 'email@test.com',
+                      username: 'new_username',
                       password1: 'P4ssword',
-                      password2: 'P4ssword',
-                      name: 'admin name',
-                      email: 'admin@test.com' }
+                      password2: 'P4ssword' }
 
     assert_equal 302, last_response.status
     get last_response["Location"]
@@ -130,11 +130,11 @@ class RAOKTest < Minitest::Test
   end
 
   def test_signin_success
-    create_admin_user
+    create_user
     current_user = '1'
-    message = '<p>admin is signed in!'
+    message = '<p>username is signed in!'
 
-    post '/signin', { username: 'admin', password: 'P4ssword' }
+    post '/signin', { username: 'username', password: 'P4ssword' }
 
     assert_equal 302, last_response.status
     get last_response["Location"]
@@ -144,10 +144,10 @@ class RAOKTest < Minitest::Test
   end
 
   def test_signin_fail_invalid_credentials
-    create_admin_user
+    create_user
     message = '<p>Invalid credentials'
 
-    # user isn't found
+    # username isn't found
     post '/signin', { username: 'fake-name', password: 'P4ssword' }
 
     assert_equal 302, last_response.status
@@ -163,6 +163,7 @@ class RAOKTest < Minitest::Test
   end
 
   def test_can_view_profile
+    create_user
 
     # signin
     # current_user = '1'
